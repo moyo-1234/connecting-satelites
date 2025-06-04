4ccb9bdccbecc57c2ae2bc2eed550f175501a734
moyo-1234 <folake.abudiore@gmail.com>
2025-06-04 18:56:22 +0100

    Add files via upload

--- cw.py
+++ cw.py
@@ -8,20 +8,32 @@ WIDTH = 700
 satelites = []
 lines = []
 satelite_num = 8
-
+next_satelite = 0
 
 def draw():
     screen.blit("background", (0,0))
+    m = 1
+    for i in satelites:
+        i.draw()
+        screen.draw.text(str(m),(i.pos[0],i.pos[1]+20))
+        m=m+1
 
 
-def create_staelite():
+def create_satelite():
     for i in range(satelite_num):
         s = Actor("satelite")
         s.pos = randint(50,640),randint(50,680)
         satelites.append(s)
 
+def on_mouse_down(pos):
+    global next_satelite,lines
+    if next_satelite < satelite_num:
+        if satelites[next_satelite].collidepoint(pos):
+            lines.append(satelites[next_satelite-1].pos,satelites[next_satelite].pos)
+            next_satelite=next_satelite+1
+        
 
-
+create_satelite()
 
 
 
